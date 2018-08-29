d4257189232adb9553fda990ffb109762b90504d
Ionut Andonescu <andonescu@gmail.com>
2018-08-29 22:09:55 +0300

    additional test for IpsController#findAll method

--- src/test/kotlin/ro/andonescu/playground/apigateway/controllers/IpsControllerTest.kt
+++ src/test/kotlin/ro/andonescu/playground/apigateway/controllers/IpsControllerTest.kt
@@ -26,12 +26,15 @@ class IpsControllerTest(@Autowired val mockMvc: MockMvc) {
     private val databaseStorage: DatabaseStorage? = null
 
 
+    private val pageNoOne = 1
+    private val defaultPageSize = 10
+
     @Test
     @DisplayName("IpsControllerTest#findAll should return all IPS from a specific page")
     fun findAll_forSpecificPageAndSize() {
         // given
         val ips = listOf("192.158.1.4", "192.158.1.2")
-        Mockito.`when`(databaseStorage?.findAll(anyInt(), anyInt())).thenReturn(Pair(ips, ips.size))
+        Mockito.`when`(databaseStorage?.findAll(defaultPageSize, pageNoOne)).thenReturn(Pair(ips, ips.size))
 
         // when
         val mvcResult: MvcResult = mockMvc.perform(get("/api/ips"))
@@ -41,14 +44,38 @@ class IpsControllerTest(@Autowired val mockMvc: MockMvc) {
         // then
         val result: ResultActions = this.mockMvc.perform(asyncDispatch(mvcResult))
 
-        println(result.andReturn().response.contentAsString)
+        result
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.pageInfo.page").value(pageNoOne))
+                .andExpect(jsonPath("$.pageInfo.total").value(ips.size))
+                .andExpect(jsonPath("$.pageInfo.size").value(defaultPageSize))
+                .andExpect(jsonPath("$.data", hasSize<Any>(ips.size)))
+    }
+
+    @Test
+    @DisplayName("IpsControllerTest#findAll should return an empty list when page requested is outside the range")
+    fun findAll_forEmptyList() {
+        // given
+        val pageNo100 = 100
+        val maxIps = 20
+        Mockito.`when`(databaseStorage?.findAll(10, pageNo100)).thenReturn(Pair(listOf(), maxIps))
+
+        // when
+        val mvcResult: MvcResult = mockMvc.perform(get("/api/ips?page=$pageNo100"))
+                .andExpect(request().asyncStarted())
+                .andReturn()
+
+        // then
+        val result: ResultActions = this.mockMvc.perform(asyncDispatch(mvcResult))
 
         result
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.pageInfo.page").value("1"))
-                .andExpect(jsonPath("$.pageInfo.total").value(ips.size))
-                .andExpect(jsonPath("$.data", hasSize<Any>(ips.size)))
+                .andExpect(jsonPath("$.pageInfo.page").value(pageNo100))
+                .andExpect(jsonPath("$.pageInfo.total").value(maxIps))
+                .andExpect(jsonPath("$.pageInfo.size").value(10))
+                .andExpect(jsonPath("$.data", hasSize<Any>(0)))
 
     }
 }
